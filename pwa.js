2df4582c5284ed75ec67bcfd6326977615ac36b0
Satish Kumar Sharma <skwebserv@gmail.com>
2023-09-07 17:54:46 +0530

    fied: dir of sw

--- pwa.js
+++ pwa.js
@@ -23,7 +23,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
+  navigator.serviceWorker.register('./service-worker.js')
     .then(registration => {
       console.log('Service Worker registered with scope:', registration.scope);
     })
